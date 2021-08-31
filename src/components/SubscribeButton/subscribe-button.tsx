29c5b05a24f8ef03656ffb884b9f7e3de45b6f3e
Grasiela Gomes <produtoradama@gmail.com>
2021-08-31 20:36:35 -0300

    creating user in stripe

--- src/components/SubscribeButton/subscribe-button.tsx
+++ src/components/SubscribeButton/subscribe-button.tsx
@@ -1,12 +1,35 @@
+import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./subscribe-button-styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  async function handleSubscription() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscription}
+    >
       Subscribe now
     </button>
   );
