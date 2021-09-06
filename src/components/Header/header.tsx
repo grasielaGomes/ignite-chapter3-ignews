7dd5729c4b1a467964b82ffd6bec6297e2bec2e7
Grasiela Gomes <produtoradama@gmail.com>
2021-09-06 15:51:12 -0300

    Refactoring code

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import { SignInButton } from '../SignInButton/sign-in-button';
 import logoImg from '../../../public/images/logo.svg';
